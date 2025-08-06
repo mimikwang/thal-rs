219b23c114304d15189b90d6dc440c9eadef4003
Mimi Wang <mimikwang@gmail.com>
2025-08-06 14:19:11 -0700

    Add thermo struct
    Added thermo struct to hold entropy and enthalpy information.

---
+++ src/thermo.rs
@@ -0,0 +1,59 @@
+/// Holds enthalpy and entropy data
+#[derive(Default, Debug, Copy, Clone, PartialEq)]
+pub struct Thermo {
+    /// Entropy
+    pub ds: f64,
+    /// Enthalpy
+    pub dh: f64,
+}
+
+impl Thermo {
+    /// Default constructor
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Construct with values
+    pub fn with_values(ds: f64, dh: f64) -> Self {
+        Self { ds, dh }
+    }
+
+    /// Calculate the gibbs free energy
+    pub fn dg(&self, temp: f64) -> f64 {
+        self.dh - temp * self.ds
+    }
+}
+
+impl std::ops::Add for Thermo {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            ds: self.ds + rhs.ds,
+            dh: self.dh + rhs.dh,
+        }
+    }
+}
+
+impl std::ops::AddAssign for Thermo {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl std::ops::Sub for Thermo {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            ds: self.ds - rhs.ds,
+            dh: self.dh - rhs.dh,
+        }
+    }
+}
+
+impl std::ops::SubAssign for Thermo {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
