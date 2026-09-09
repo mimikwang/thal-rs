88a0339567a9412c09ae51e40c73a5a545853ce0
Mimi Wang <mimikwang@gmail.com>
2026-09-09 02:23:08 -0700

    Add CLI to main
    Added CLI to main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use crate::{errors::Result, mfe::Dimer, thermo::params::ThermoParams};
 
 mod errors;
@@ -8,9 +10,21 @@ mod thal;
 mod thermo;
 
 fn main() -> Result<()> {
-    let params = ThermoParams::with_file_path("thermo")?;
-    let mut mfe = Dimer::new(b"AGCT", b"TCGA", &params)?;
-    println!("{:?}", mfe.calculate()?);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        return Err("invalid args");
+    }
 
-    Ok(())
+    if &args[1] == "dimer" {
+        let seq1 = &args[2];
+        let seq2 = &args[3];
+
+        let params = ThermoParams::with_file_path("thermo")?;
+        let mut mfe = Dimer::new(seq1.as_bytes(), seq2.as_bytes(), &params)?;
+        println!("{:?}", mfe.calculate()?);
+
+        return Ok(());
+    }
+
+    Err("invalid args")
 }
